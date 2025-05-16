c26c1b84586588491cf384af11d7e32d69e5a86c
Isaac <47983542+isaac-w-dev@users.noreply.github.com>
2025-05-16 10:32:41 -0400

    Finished design system for resume page to be fully implemented in the next push

--- src/components/Resume/GeneralTips.jsx
+++ src/components/Resume/GeneralTips.jsx
@@ -19,15 +19,23 @@ const GeneralTips = (props) => {
         <div className={getCategories.customization ? 'subcategory white' : 'subcategory'}>
           <button className='subcategory-button' name='customization' onClick={(e) => changeHandler(e, getCategories, setCategories)}>Customize Resume for Each Employer</button>
           {getCategories.customization ?
-            <div>
+            <div className='subcategory-content'>
               {getSubCategories.keywords ?
-                <div className='subcategory-item'>
-                  <button className='subcategory-item-button' onClick={(e) => changeHandler(e, getSubCategories, setSubCategories)}>Include Keywords in Your Resume</button>
+                <div className='subcategory-item teal'>
+                  <button name='keywords' className='subcategory-item-button' onClick={(e) => changeHandler(e, getSubCategories, setSubCategories)}>Include Keywords in Your Resume</button>
+                  <div className='subcategory-item-content'>
+                    <h1 className='white bullet'>Minimum Qualifications</h1>
+                    <h1 className='white bullet'>Mindset</h1>
+                    <h1 className='white bullet'>Preferred but not required</h1>
+                    <h1 className='white bullet'>Values Stated on the company website</h1>
+                    <h1 className='white bullet'>Include company name and job title</h1>
+                    <h1 className='white bullet'>Name resume after company: ex. 	Resume_Company_Position_YourName</h1>
+                  </div>
                 </div>
                 :
-                <button className='subcategory-item-button' onClick={(e) => changeHandler(e, getSubCategories, setSubCategories)}>Include Keywords in Your Resume</button>
+                <button name='keywords' className='subcategory-item-button' onClick={(e) => changeHandler(e, getSubCategories, setSubCategories)}>Include Keywords in Your Resume</button>
               }
-              <button className='subcategory-item-button'>Include </button>
+              <button name='additional' onClick={(e) => changeHandler(e, getSubCategories, setSubCategories)}className='subcategory-item-button'>Additional Customization Tips</button>
             </div>
             :
             <br />
